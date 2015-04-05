0007708653ee1c9656ac687bd1f8b8b3c31792cd
Thanh <thanh@Thanhs-MacBook-Air.local>
2015-04-04 18:58:12 -0700

    refactor and module.export

--- httpRequest/app.js
+++ httpRequest/app.js
@@ -1,50 +1,5 @@
-/*
-to make a http GET call to my teamtreehouse account (set to public)
-return the JSON
+/**
+ * this will get a teamtreehouse user profile
  */
-var http = require('http');
-var username = 'ithanhpham';
-
-//Main message
-function printMessage(username, badgeCount, points) {
-    var message = username + ' has ' + badgeCount + ' total badge(s) and ' + points + ' points in JavaScript.';
-    console.log(message);
-}
-
-//Error handling function
-function printError(error) {
-    console.error('Error: ' + error.message);
-}
-
-//connect to API (http://teamtreehouse.com/ithanhpham.json)
-var request = http.get('http://teamtreehouse.com/' + username + '.json', function(response) {
-    var body = "";
-
-    console.log('Status: ' + response.statusCode)
-
-        //read data, concat during the stream
-        response.on('data', function (chunk) {
-            body += chunk;
-        });
-
-        response.on('end', function () {
-            if (response.statusCode == 200) {
-                try {
-                    var profile = JSON.parse(body);
-                    printMessage(username, profile.badges.length, profile.points.JavaScript);
-
-                } catch (error) {
-                    //parse error
-                    printError(error);
-                }
-            } else {
-                printError({message : "There was an error trying to get the URL for " + username + ". (" + http.STATUS_CODES[response.statusCode] + ")" });
-
-            }
-        });
-});
-
-//connection error
-request.on('error', function(error) {
-    console.error(printError);
-});
+var profile = require('./profile');
+profile.get('ithanhpham');
---
+++ httpRequest/profile.js
@@ -0,0 +1,53 @@
+/**
+ * get user profile:
+ * make a http GET call to my teamtreehouse account (set to public)
+ */
+
+var http = require('http');
+
+//Main message
+function printMessage(username, badgeCount, points) {
+    var message = username + ' has ' + badgeCount + ' total badge(s) and ' + points + ' points in JavaScript.';
+    console.log(message);
+}
+
+//Error handling function
+function printError(error) {
+    console.error('Error: ' + error.message);
+}
+
+//connect to API (http://teamtreehouse.com/ithanhpham.json)
+function get(username) {
+    var request = http.get('http://teamtreehouse.com/' + username + '.json', function (response) {
+        var body = "";
+
+        console.log('Status: ' + response.statusCode)
+
+        //read data, concat during the stream
+        response.on('data', function (chunk) {
+            body += chunk;
+        });
+
+        response.on('end', function () {
+            if (response.statusCode == 200) {
+                try {
+                    var profile = JSON.parse(body);
+                    printMessage(username, profile.badges.length, profile.points.JavaScript);
+
+                } catch (error) {
+                    //parse error
+                    printError(error);
+                }
+            } else {
+                printError({message: "There was an error trying to get the URL for " + username + ". (" + http.STATUS_CODES[response.statusCode] + ")"});
+
+            }
+        });
+    });
+
+    //connection error
+    request.on('error', printError);
+
+}
+
+module.exports.get = get;
